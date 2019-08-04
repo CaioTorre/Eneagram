2d206045f675e25a7f0cad50eac0ce0691e7b56d
Linksya <caiosanches@live.com>
2019-08-03 22:10:47 -0300

    Rebuilt Exceptions
    Now points out row/column where there was a mistake;
    Code cleanup

--- nana.py
+++ nana.py
@@ -6,21 +6,30 @@ IS_PAINT = 1
 IS_BLANK = 0
 IS_CROSS = -1
 
-# Defining helper class
-class ColisionError(Exception):
+DEBUG = False
+
+# Defining helper classes
+class PuzzleException(Exception):
     pass
+    
+class ColisionError(PuzzleException):
+    pass
+    
+class NoOptionsError(PuzzleException):
+    def __init__(self, row_or_column, number):
+        super().__init__("{} {} has no fitting combinations left!".format(row_or_column, number))
+        
     
 clues_horz = []
 clues_vert = []
 
 # Read puzzle from file
-puzzle_file = "pokemon_x.non"
+puzzle_file = "puzzles\pokemon_x.non"
 with open(puzzle_file, "r") as f:
     reading = 0
     #buff = 0
     for line in f:
         line_tokens = line.split(' ')
-        #print(line_tokens)
         if (len(line_tokens) == 0) or (line_tokens[0] == '\n'): #buff == 0 and 
             reading = 0
         else:    
@@ -31,10 +40,8 @@ with open(puzzle_file, "r") as f:
                     h = int(line_tokens[1])
                 elif line_tokens[0] == 'rows' or line_tokens[0] == 'rows\n':
                     reading = 1
-                    #buff = 1
                 elif line_tokens[0] == 'columns' or line_tokens[0] == 'columns\n':
                     reading = 2
-                    #buff = 1
                 else:
                     pass
             elif reading == 1: # rows
@@ -43,17 +50,15 @@ with open(puzzle_file, "r") as f:
                     clues_horz.append([int(v) for v in line[:-1].split(',')])
                 else:
                     clues_horz.append([int(v) for v in line.split(',')])
-                #buff = 0
             elif reading == 2: # columns
                 if line[-1] == '\n':
                     clues_vert.append([int(v) for v in line[:-1].split(',')])
                 else:
                     clues_vert.append([int(v) for v in line.split(',')])
-                #clues_vert.append([int(v) for v in line[:-1].split(',')])
-                #buff = 0
-                    
-print(clues_horz)
-print(clues_vert)
+
+if DEBUG:                    
+    print(clues_horz)
+    print(clues_vert)
 
 # Defining function to print board
 def present_board(board):
@@ -65,14 +70,13 @@ def present_board(board):
         print('\t' + '|'.join(line_trans))
 
 # Define the board size
-#board_size = (9,8) # Lines, columns
 board_size = (h,w)
 
 # Sets of clues (will be read from file?) 
 #clues_horz = [[3],[2,1],[3,2],[2,2],[6],[1,5],[6],[1],[2]]
 #clues_vert = [[1,2],[3,1],[1,5],[7,1],[5],[3],[4],[3]]
 
-# Format reading = http://scc-forge.lancaster.ac.uk/open/nonogram/fmt2
+# Format reading = http://scc-forge.lancaster.ac.uk/open/nonogram/fmt2 ; https://github.com/mikix/nonogram-db/blob/master/FORMAT.md
 
 # Make sure we have clues for all lines and columns
 assert len(clues_horz) == board_size[0], "Expected {} horizontal clues, got {}".format(board_size[0], len(clues_horz))
@@ -84,9 +88,6 @@ board += IS_BLANK
 
 # Create all possible lines and columns given the clues and the board size
 def partitions(n, t):
-    """
-    Generate the sequences of `n` positive integers that sum to `t`.
-    """
     assert(0 <= n <= t)
     def intervals(c):
         last = 0
@@ -107,8 +108,6 @@ def create_list(width, clues):
             res += [IS_PAINT] * clues[i + 1]
         res += [IS_CROSS] * (space_sequence[-1] - 1) # *Here too
         yield res
-    
-print(list(create_list(3,[1,1])))
 
 # Creating lines
 def create_all_1d(width, all_clues):
@@ -120,71 +119,82 @@ def create_all_1d(width, all_clues):
 track_horz = list(create_all_1d(board_size[1], clues_horz))
 track_vert = list(create_all_1d(board_size[0], clues_vert))
 
-print(track_horz)
-print(track_vert)
+if DEBUG:
+    print(track_horz)
+    print(track_vert)
 
 def check_colision(current_state, match):
-    #print("checking " + str(current_state) + " vs " + str(match))
     assert len(current_state) == len(match)
     for i in range(len(current_state)):
         if current_state[i] != IS_BLANK and match[i] != IS_BLANK and current_state[i] != match[i]:
-            #print("failed...")
             raise ColisionError
 
 stalemate = False
 victory = False
 while (not stalemate) and (not victory):
-    _last_board_state = np.copy(board)
-    # For each column, check if fits given current board state
-    for column_match_i in range(len(track_vert)):
-        _pop_list = []
-        for indiv_match_i in range(len(track_vert[column_match_i])):
-            try:
-                check_colision(board[:,column_match_i], track_vert[column_match_i][indiv_match_i])
-            except ColisionError:
-                _pop_list = [indiv_match_i] + _pop_list
-        # Remove all that dont fit current board state
-        for _pop_id in _pop_list:
-            track_vert[column_match_i].pop(_pop_id)
-            
-        # Given the surviving options, find intersections between them
-        _new_option = track_vert[column_match_i][0]
-        if len(track_vert[column_match_i]) > 0:
-            for surviving_match in track_vert[column_match_i][1:]:
-                _new_option = np.where(_new_option == np.array(surviving_match), surviving_match, IS_BLANK)
-            
-        board[:,column_match_i] = _new_option
-        #print("New column {} = {}".format(column_match_i, str(_new_option)))
-    print("Board after column analysis:")
-    present_board(board) 
-    
-    # For each row, check if fits given current board state
-    for row_match_i in range(len(track_horz)):
-        _pop_list = []
-        for indiv_match_i in range(len(track_horz[row_match_i])):
-            try:
-                check_colision(board[row_match_i,:], track_horz[row_match_i][indiv_match_i])
-            except ColisionError:
-                _pop_list = [indiv_match_i] + _pop_list
-        # Remove all that dont fit current board state
-        for _pop_id in _pop_list:
-            track_horz[row_match_i].pop(_pop_id)
-            
-        # Given the surviving options, find intersections between them
-        _new_option = track_horz[row_match_i][0]
-        if len(track_horz[row_match_i]) > 0:
-            for surviving_match in track_horz[row_match_i][1:]:
-                _new_option = np.where(_new_option == np.array(surviving_match), surviving_match, IS_BLANK)
+    try:
+        _last_board_state = np.copy(board)
+        # For each column, check if fits given current board state
+        for column_match_i in range(len(track_vert)):
+            _pop_list = []
+            for indiv_match_i in range(len(track_vert[column_match_i])):
+                try:
+                    check_colision(board[:,column_match_i], track_vert[column_match_i][indiv_match_i])
+                except ColisionError:
+                    _pop_list = [indiv_match_i] + _pop_list
+            # Remove all that dont fit current board state
+            for _pop_id in _pop_list:
+                track_vert[column_match_i].pop(_pop_id)
+
+            if len(track_vert[column_match_i]) == 0: # Error
+                raise NoOptionsError("Column", column_match_i + 1)
+                
+            # Given the surviving options, find intersections between them
+            _new_option = track_vert[column_match_i][0]
+            if len(track_vert[column_match_i]) > 0:
+                for surviving_match in track_vert[column_match_i][1:]:
+                    _new_option = np.where(_new_option == np.array(surviving_match), surviving_match, IS_BLANK)
+                
+            board[:,column_match_i] = _new_option
+        print("Board after column analysis:")
+        present_board(board) 
+        print("----------------------------")
         
-        board[row_match_i,:] = _new_option
-    print("Board after row analysis:")
-    present_board(board) 
-    
-    if not np.any(board != _last_board_state):
-        stalemate = True
-        
-    if not np.any(board == IS_BLANK):
-        victory = True
-        
+        # For each row, check if fits given current board state
+        for row_match_i in range(len(track_horz)):
+            _pop_list = []
+            for indiv_match_i in range(len(track_horz[row_match_i])):
+                try:
+                    check_colision(board[row_match_i,:], track_horz[row_match_i][indiv_match_i])
+                except ColisionError:
+                    _pop_list = [indiv_match_i] + _pop_list
+            # Remove all that dont fit current board state
+            for _pop_id in _pop_list:
+                track_horz[row_match_i].pop(_pop_id)
+
+            if len(track_horz[row_match_i]) == 0: # Error
+                raise NoOptionsError("Row", row_match_i + 1)
+                
+            # Given the surviving options, find intersections between them
+            _new_option = track_horz[row_match_i][0]
+            if len(track_horz[row_match_i]) > 0:
+                for surviving_match in track_horz[row_match_i][1:]:
+                    _new_option = np.where(_new_option == np.array(surviving_match), surviving_match, IS_BLANK)
+            
+            board[row_match_i,:] = _new_option
+        print("Board after row analysis:")
+        present_board(board) 
+        print("----------------------------")
+        if not np.any(board != _last_board_state):
+            stalemate = True
+        if not np.any(board == IS_BLANK):
+            victory = True
+    except NoOptionsError as e:
+        print(e)
+        print("Check if the clues are correct then try again.")
+        break
+    except Exception:
+        print("Unknown error. Maybe puzzle is not solvable? Check if the clues are correct then try again.")
+        break
 if stalemate:
-    print("Board state did not change!")
+    print("Error: Board state did not change!")
